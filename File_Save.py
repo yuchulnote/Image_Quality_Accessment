8c6ddc47b36d0da8b52d6cb37c91f23ad951439d
Jung-Yuchul <108872871+yuchulnote@users.noreply.github.com>
2023-07-26 17:46:16 +0900

    Update File_Save.py
    세부사항 변경

--- File_Save.py
+++ File_Save.py
@@ -3,13 +3,13 @@ import shutil
 import pandas as pd
 
 # 시작 디렉토리 경로
-start_dir = "C:/Users/PETNOW/Desktop/shadow_glare_motionblur"  # 이 경로를 실제 시작 디렉토리 경로로 변경
+start_dir = ""  # 이 경로를 실제 시작 디렉토리 경로로 변경
 
 # 결과 파일들을 저장할 디렉토리 경로
-dest_dir = "C:/Users/PETNOW/Desktop/glar_positive_motionblur_negative"  # 이 경로를 실제 결과 파일을 저장할 디렉토리 경로로 변경
+dest_dir = ""
 
 # CSV 파일 경로
-csv_file = "C:/Users/PETNOW/Desktop/glar_positive_motionblur_negative/image_name.csv"  # 이 경로를 실제 CSV 파일 경로로 변경
+csv_file = ""  # 이 경로를 실제 CSV 파일 경로로 변경
 
 # CSV 파일 읽어오기
 df = pd.read_csv(csv_file)
